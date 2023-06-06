24b13cb7f9b569b647609cc868a8529044f9817e
Tejus <tejusmdn@gmail.com>
2023-06-06 17:17:19 +1000

    Added attributes for serialization

--- CafeCommon/Models/Drink.cs
+++ CafeCommon/Models/Drink.cs
@@ -5,12 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CafeCommon.Enums;
+using Newtonsoft.Json;
 
 namespace CafeCommon.Models
 {
     public class Drink
     {
+        [JsonProperty("size")]
         public DrinkSize Size { get; set; }
+
+        [JsonProperty("type")]
         public DrinkType Type { get; set; }
     }
 }
--- CafeCommon/Models/Order.cs
+++ CafeCommon/Models/Order.cs
@@ -1,13 +1,23 @@
 ﻿using CafeCommon.Enums;
+using Newtonsoft.Json;
 
 namespace CafeCommon.Models
 {
     public class Order
     {
+        [JsonProperty("id")]
         public string Id { get; set; }
+
+        [JsonProperty("name")]
         public string Name { get; set; }
+
+        [JsonProperty("createdDate")]
         public DateTime CreatedDate { get; set; }
+
+        [JsonProperty("drinks")]
         public IEnumerable<Drink> Drinks { get; set; } = new List<Drink>();
+
+        [JsonProperty("state")]
         public OrderState State { get; set; }
     }
 }
